3991e55501fbb76dea6f0c464d06b2f28242bbe4
ridwaanhall <erbyl14@gmail.com>
2024-06-25 22:42:16 +0700

    added base/templates dirs

--- khodam/settings.py
+++ khodam/settings.py
@@ -55,7 +55,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates'
+            BASE_DIR / 'templates',
+            BASE_DIR / 'base/templates',
             ],
         'APP_DIRS': True,
         'OPTIONS': {
