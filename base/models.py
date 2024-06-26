92eb7efc0d336f6f38dfa9ac4722f9f88bd9faf5
ridwaanhall <erbyl14@gmail.com>
2024-06-26 22:43:32 +0700

    create table

--- base/models.py
+++ base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class ListKhodam(models.Model):
+    id = models.AutoField(primary_key=True)
+    nama_khodam = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.nama_khodam
