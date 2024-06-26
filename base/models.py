6c542fa26c857fc35682082bdc83139c0f5e14c5
ridwaanhall <erbyl14@gmail.com>
2024-06-27 00:53:43 +0700

    create response log

--- base/models.py
+++ base/models.py
@@ -7,3 +7,14 @@ class ListKhodam(models.Model):
 
     def __str__(self):
         return self.nama_khodam
+
+class ResponseLog(models.Model):
+    code = models.IntegerField()
+    status = models.CharField(max_length=100)
+    message = models.TextField()
+    name = models.CharField(max_length=255, null=True, blank=True)
+    khodam = models.CharField(max_length=255, null=True, blank=True)
+    current_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.current_time} - {self.name} - {self.khodam}"
