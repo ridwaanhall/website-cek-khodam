bcda41e46d064060317e7f285658f29d4f1cc925
ridwaanhall <erbyl14@gmail.com>
2024-06-27 00:53:56 +0700

    modify path

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('input/', views.input, name='input'),
+    path('cek-khodam/', views.cek_khodam, name='cek-khodam'),
 ]
