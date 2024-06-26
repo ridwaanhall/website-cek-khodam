153a3e06e15906bd611d8bc484b3164be1ae1e8c
ridwaanhall <erbyl14@gmail.com>
2024-06-26 23:20:56 +0700

    add date time and call list of khodams

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from base.models import ListKhodam
 import random
+from datetime import datetime
 
 def home(request):
     return render(request, 'base/cek-khodam.html')
@@ -8,7 +10,14 @@ def home(request):
 def input(request):
     if request.method == 'POST':
         name = request.POST.get('name-username', '')
-        responses = ["batu kodok", "lalat", "pintu"]
-        response = random.choice(responses)
-        return JsonResponse({'name': name, 'response': response})
+        
+        khodams = list(ListKhodam.objects.values_list('nama_khodam', flat=True))
+        
+        if khodams:
+            response = random.choice(khodams)
+            current_time = datetime.now().strftime('%Y%m%d%H%M%S')
+            return JsonResponse({'name': name, 'response': response, 'datetime_now': current_time})
+        else:
+            return JsonResponse({'response': 'No khodam data available'}, status=404)
+    
     return JsonResponse({'response': 'Invalid request'}, status=400)
